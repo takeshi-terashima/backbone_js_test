0fbf5a36ea76a92d996eac534ae46f5317cb1c4c
takeshi-terashima <history0912tkc@gmail.com>
2021-08-03 13:16:46 +0900

    [Add] file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 (function() {
 
-});
+    //MOdel
+
+    var Task = Backbone.Model.extend({
+        defaults: {
+            title: "do something!",
+            completed: false
+        }
+    });
+
+    var task1 = new Task({
+        title: 'do it!',
+        completed: true
+    });
+
+    console.log(task1.toJSON());
+
+})();
